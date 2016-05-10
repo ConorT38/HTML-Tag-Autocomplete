14bfde517bf0c35c599e61c49605ee9e1572e9ca
Conor Thompson <conorthompson213@gmail.com>
2016-05-10 07:30:28 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).keydown(function(){
+var open_tags = ["<p>","<b>","<h2>","<code>"];
+var closed_tags = ["</p>","</b>","</h2>","</code>"];
+
+for(var i =0; i<open_tags.length; i++){
+var first=open_tags[i], end=" "+open_tags[i]+closed_tags[i]+" ";
+	for(var j=3; j<8;j++){
+  	if($('#input').val().slice(-j) == first){
+       $('#input').val($('#input').val().replace($('#input').val().slice(-j),end));
+    }
+  }
+}
+});
